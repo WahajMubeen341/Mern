936d7da19ea1917fe97163f9e54c5015f5e1ac61
WahajMubeen341 <iwahajmubeen341@gmail.com>
2021-11-08 16:34:31 +0500

    Calculator project completed

--- CalculatorProject/index.js
+++ CalculatorProject/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+//const { urlencoded } = require('body-parser');
+
+app.use(express.urlencoded({extended: true}));
 
 app.listen(3000, function(){
-    console.log('your server has started at port 3000');
+    console.log('Your server has started at port 3000');
 });
 
 app.get('/', function(req, res){
 
     res.sendFile(__dirname+'/index.html');
 });
+
+app.post('/', function(req, res){
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var answer = num1 - num2;
+    res.send("<h1>Thanks for posting</h1> Your answer is : "+answer);
+})
