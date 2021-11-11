d671dc6cb5ffffbeb929d907a095e9e207198a72
WahajMubeen341 <iwahajmubeen341@gmail.com>
2021-11-11 11:45:02 +0500

    BMI calculator done in Calculator Project

--- CalculatorProject/index.js
+++ CalculatorProject/index.js
@@ -20,4 +20,27 @@ app.post('/', function(req, res){
     var num2 = Number(req.body.num2);
     var answer = num1 - num2;
     res.send("<h1>Thanks for posting</h1> Your answer is : "+answer);
-})
+});
+
+
+app.get('/bmi', function(req, res){
+    res.sendFile(__dirname+'/bmiCalculator.html');
+});
+
+app.post('/bmi', function(req, res){
+    
+    var heightFeat =  Number(req.body.feets);
+
+    var featToInchesHeight = heightFeat*12; 
+    var heightInch =  Number(req.body.inches);
+    heightInch += featToInchesHeight;
+    var weightKG =  Number(req.body.weight);
+
+    var answer = weightKG/(heightInch*heightInch)*703;
+    answer = answer.toFixed(2);  
+
+    console.log("answer: "+ answer);
+
+    res.send("<h1>Your BMI is: {answer}</h1>" );
+
+});
